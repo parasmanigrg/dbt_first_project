3d9275d75fc827cadc2bb8d95e2c16fde5a34c0f
pmanigrg@gmail.com <pmanigrg@gmail.com>
2025-08-31 18:16:23 +0000

    data freshness check added

---
+++ models/mart/finance/fact_orders.sql
@@ -0,0 +1,20 @@
+with orders as(
+Select *
+From {{ref('stg_orders')}}
+)
+, payments as(
+    Select*
+    From {{ref('stag_payments')}}
+
+)
+, final as(
+Select o.order_id,
+        o.customer_id,
+        o.order_date,
+        p.amount
+From orders o 
+Left Join payments p On o.order_id=p.order_id
+)
+
+Select *
+From final 
